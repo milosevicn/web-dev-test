3b2019868a8088067b461d9ce34ab40bed04a2da
milosevicn <fonnikola@gmail.com>
2021-01-22 07:14:24 +0100

    Comment controller, model and relationships

---
+++ database/migrations/2021_01_21_161048_create_comments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->integer('post_id');
+            $table->integer('parent_id')->nullable();
+            $table->string('name');
+            $table->string('email');
+            $table->text('comment');
+            $table->boolean('approved');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
